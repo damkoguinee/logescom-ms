e7da89097db902d1d70f5822b2beca78b8954e63
damko <d.amadoumouctar@yahoo.fr>
2023-09-19 17:54:51 +0200

    first commit

---
+++ navfournisseur.php
@@ -0,0 +1,14 @@
+<div class="col-sm-12 col-md-2 pb-3" style="background-color:#2f2c2c; ">
+    
+    <div class="row mt-3">
+        <div class="col" ><a style="width: 100%; " class="btn btn-light text-center fw-bold" href="editionfacturefournisseur.php">Factures des Fournisseurs</a></div>
+    </div>
+
+    <!-- <div class="row mt-3">
+        <div class="col" ><a style="width: 100%; " class="btn btn-light text-center fw-bold" href="chequeespeces.php?cheques">Cheque/Espèces</a></div>
+    </div> -->
+
+    <div class="row mt-3">
+        <div class="col" ><a style="width: 100%; " class="btn btn-light text-center fw-bold" href="bon_commande.php">Bon de commande</a></div>
+    </div>
+</div>
